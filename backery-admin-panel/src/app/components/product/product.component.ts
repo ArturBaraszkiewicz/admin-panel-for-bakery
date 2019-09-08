49a81f09889910740a7906db476faa03f7299085
unknown <abaraszkiewicz@kogifi.com>
2019-09-08 21:29:24 +0200

    add input to product

--- backery-admin-panel/src/app/components/product/product.component.ts
+++ backery-admin-panel/src/app/components/product/product.component.ts
@@ -1,14 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
+
+export interface IProductData {
+  id: number;
+  name?: string;
+  description?: string;
+  price?: number;
+  portionsNumber?: number;
+}
 
 @Component({
   selector: 'product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.scss']
+  styleUrls: ['./product.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProductComponent implements OnInit {
 
+  /**
+   * Id product we want to display
+   */
+  @Input()
+  productId: number;
+
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
 }
